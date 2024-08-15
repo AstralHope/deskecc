9550d6514e6553db2da3424f68e424d27cca3aa7
Astral <astralhope@gmail.com>
2024-08-16 01:07:48 +0800

    cluster2

--- mid/clusterinfo.php
+++ mid/clusterinfo.php
@@ -21,22 +21,66 @@
 // 指定目录路径
 $dir = "/data/deskecc/ack/clusterinfo/";
 
-// 打开指定目录
-if (is_dir($dir)) {
-    // 打开目录
-    if ($dh = opendir($dir)) {
-        // 循环读取目录中的文件
-        while (($file = readdir($dh)) !== false) {
-            // 仅处理 .csv 文件
-            if (pathinfo($file, PATHINFO_EXTENSION) === 'csv') {
-                // 输出超链接
-                echo "<a href='view_csv.php?file=" . urlencode($file) . "'>" . $file . "</a><br>";
+// 遍历根目录及其一级子目录中的所有 .txt 和 .csv 文件
+function getFiles($dir) {
+    $files = [];
+    foreach (scandir($dir) as $item) {
+        if ($item == '.' || $item == '..') continue;
+        $filePath = $dir . '/' . $item;
+        if (is_file($filePath)) {
+            $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+            if ($extension === 'txt' || $extension === 'csv') {
+                $files[] = $filePath;
+            }
+        } elseif (is_dir($filePath)) {
+            // 仅遍历一级子目录
+            foreach (scandir($filePath) as $subItem) {
+                if ($subItem == '.' || $subItem == '..') continue;
+                $subFilePath = $filePath . '/' . $subItem;
+                if (is_file($subFilePath)) {
+                    $extension = pathinfo($subFilePath, PATHINFO_EXTENSION);
+                    if ($extension === 'txt' || $extension === 'csv') {
+                        $files[] = $subFilePath;
+                    }
+                }
             }
         }
-        // 关闭目录句柄
-        closedir($dh);
     }
+    return $files;
 }
+
+// 获取所有 .txt 和 .csv 文件
+$Files = getFiles($dir);
+
+if (!empty($Files)) {
+    echo "<ul>";
+    foreach ($Files as $file) {
+        // 生成相对路径链接
+        $relativePath = str_replace($rootDir . '/', '', $file);
+        echo "<li><a href='view_csv.php?file=" . urlencode($relativePath) . "'>" . htmlspecialchars($relativePath) . "</a></li>";
+    }
+    echo "</ul>";
+} else {
+    echo "指定目录及其一级子目录中没有找到 .txt 或 .csv 文件。";
+}
+
+
+// // 打开指定目录
+// if (is_dir($dir)) {
+//     // 打开目录
+//     if ($dh = opendir($dir)) {
+//         // 循环读取目录中的文件
+//         while (($file = readdir($dh)) !== false) {
+//             // 仅处理 .csv 文件
+//             if (pathinfo($file, PATHINFO_EXTENSION) === 'csv') {
+//                 // 输出超链接
+//                 echo "<a href='view_csv.php?file=" . urlencode($file) . "'>" . $file . "</a><br>";
+//             }
+//         }
+//         // 关闭目录句柄
+//         closedir($dh);
+//     }
+// }
 ?>
 
 </body>
